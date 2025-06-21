2ff34714b20dc3c21613cded9289ee504e8c1046
raharjo39 <risqiraharjo39@gmail.com>
2025-06-21 22:09:59 +0700

    UI: Remove progress indicator dots below Mulai button on Splash page

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -12,20 +12,14 @@ const Splash = () => {
           <img 
             src="/lovable-uploads/09a504ae-e1fc-4f6c-8f29-a7f626a53e68.png" 
             alt="Team Collaboration" 
-            className="w-full h-auto max-h-[96vh] object-contain scale-[1.2]"
+            className="w-4/5 h-auto max-h-[96vh] object-contain"
           />
         </div>
         
-        <div className="flex justify-center py-4">
-          <div className="flex space-x-2">
-            <div className="w-8 h-1 bg-white rounded"></div>
-            <div className="w-2 h-1 bg-white/50 rounded"></div>
-            <div className="w-2 h-1 bg-white/50 rounded"></div>
-          </div>
-        </div>
+
       </div>
       
-      <div className="w-full max-w-md mx-auto px-6 pb-8 -translate-y-5">
+      <div className="w-full max-w-md mx-auto px-6 pb-8 -translate-y-[2cm]">
         <Button 
           onClick={() => navigate('/dashboard')}
           className="bg-white text-blue-600 hover:bg-gray-100 px-8 py-3 rounded-full font-semibold text-lg w-full"
